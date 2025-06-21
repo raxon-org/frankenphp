d9d8333c05e5bfc2f6dc0465cdaed780e20d85a3
remco van der Velde <remco.pc@outlook.com>
2025-06-21 14:37:33 +0200

    frankenphp init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -17,21 +17,14 @@ trait Main {
     public function frankenphp_install(): void
     {
         Core::interactive();
-//        $command = 'curl -fsSL https://deno.land/install.sh | sh';
-//        exec($command, $output);
-//        echo implode(PHP_EOL, $output) . PHP_EOL;
         $object = $this->object();
-        $command = 'app zip extract -source=' . $object->config('project.dir.vendor') . 'raxon/deno/src/Data/deno.zip' . ' -target=/usr/local/bin/ -patch';
-        echo $command . PHP_EOL;
+        $source = $object->config('project.dir.vendor') . 'raxon/frankenphp/src/Data/frankenphp-linux-x86_64';
+        $target = '/usr/local/bin/frankenphp';
+        File::copy($source, $target);
+        $command = 'chmod +x ' . $target;
         exec($command, $output);
         echo implode(PHP_EOL, $output) . PHP_EOL;
-        $command = 'chmod +x /usr/local/bin/deno';
-        exec($command, $output);
-        echo implode(PHP_EOL, $output) . PHP_EOL;
-//        $command = 'cp ~/.deno/bin/deno /usr/local/bin/deno';
-//        exec($command, $output);
-//        echo implode(PHP_EOL, $output) . PHP_EOL;
-        $command = 'deno --version';
+        $command = 'frankenphp --version';
         exec($command, $output);
         echo implode(PHP_EOL, $output) . PHP_EOL;
     }
